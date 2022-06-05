186b5f6e48c39518318e276ac62b0c3b33d43a32
jicu mollick <jikumollick82@gmail.com>
2022-06-05 13:20:52 +0600

    navbar done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me"></div>
+        <div className="about-content"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
